9dc44fd000e93323f4cde57e5db18587b79cbb55
scalli007YT <pascal.rose007@web.de>
2025-05-24 23:28:42 +0200

    ✨ Add opacity to icons in Modpack card

--- components/Cards/Modpack.vue
+++ components/Cards/Modpack.vue
@@ -9,14 +9,14 @@
         </CardHeader>
         <CardContent>
           <div class="flex items-center gap-2">
-            <Icon size="20" name="solar:server-2-bold" />
+            <Icon size="20" name="solar:server-2-bold" class="opacity-30" />
             <span>{{ modpack.summary }}</span>
           </div>
         </CardContent>
 
         <CardFooter>
           <div class="flex items-center gap-2">
-            <Icon size="20" name="solar:download-bold" />
+            <Icon size="20" name="solar:download-bold" class="opacity-30" />
             <span>{{ modpack.downloadCount }}</span>
           </div>
         </CardFooter>
